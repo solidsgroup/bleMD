22e061f17497ce6732ebde11a84ffb56c6c6ae81
Brandon Runnels <brunnels@uccs.edu>
2023-11-20 15:28:56 -0700

    fixed install path

--- scripts/install.py
+++ scripts/install.py
@@ -1,3 +1,3 @@
 import bpy
-bpy.ops.preferences.addon_install(filepath="/home/brunnels/Research/bleMD/bleMD.zip",overwrite=True)
+bpy.ops.preferences.addon_install(filepath="bleMD.zip",overwrite=True)
 bpy.ops.preferences.addon_enable(module='bleMD')
