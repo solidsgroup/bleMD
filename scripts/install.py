b51465eea9710767409b73b48cc6431cbbbd499a
Brandon Runnels <brunnels@uccs.edu>
2023-11-21 15:45:50 -0700

    now that action is properly failing, try again with the error fixed

--- scripts/install.py
+++ scripts/install.py
@@ -1,4 +1,4 @@
 import bpy
-bpy.ops.preferences.addon_install(filepath="bleMD.zip",overwrite=True)
+bpy.ops.preferences.addon_install(filepath="./bleMD.zip",overwrite=True)
 bpy.ops.preferences.addon_enable(module='bleMD')
 print("---successful completion---")
