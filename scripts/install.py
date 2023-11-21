ae4b02305a08f163d4515efec10219ea38034952
Brandon Runnels <brunnels@uccs.edu>
2023-11-21 15:43:29 -0700

    adding wrapper

--- scripts/install.py
+++ scripts/install.py
@@ -1,13 +1,4 @@
-try:
-    import bpy
-    bpy.ops.preferences.addon_install(filepath="bleMD.zip",overwrite=True)
-    bpy.ops.preferences.addon_enable(module='bleMD')
-
-except Exception as e:
-    print("Aha I caught an exception")
-    print(e)
-    raise e
-### You must print this line, otherwise the Github action will think 
-### the script failed.
-#print("SCRIPT COMPLETED SUCCESSFULLY")
-
+import bpy
+bpy.ops.preferences.addon_install(filepath="bleMD.zip",overwrite=True)
+bpy.ops.preferences.addon_enable(module='bleMD')
+print("---successful completion---")
---
+++ scripts/wrapper.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+
+try:
+    output = subprocess.check_output(sys.argv[1:])
+    print(output.decode('ascii'))
+    if not "---successful completion---" in output.decode('ascii'):
+        raise Exception("Script errored out")
+    print("Successful completion")
+except FileNotFoundError as e:
+    print("could not find file", e)#, e.output)
+    raise e
+except subprocess.CalledProcessError as e:
+    print("error code", e.returncode)#, e.output)
+    raise e
+except Exception as e:
+    print(e)
+    raise e
+
+print("Ah yes, definitely successful")
